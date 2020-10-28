03732923b0559045c4ad2f7c423eb220b2776ef1
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 15:46:12 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,4 @@
 module.exports.defaultRoutes = require('./default');
 module.exports.userRoutes = require('./user');
 module.exports.chatRoomRoutes = require('./chatRoom');
+module.exports.deleteRoutes = require('./delete');
