be37543b6e525a7dc1ea296bdabb25edce077297
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 16:01:21 +0530

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 module.exports.User = require('./User');
+module.exports.User_Types = require('./User').USER_TYPES;
 module.exports.ChatRoom = require('./ChatRoom');
 module.exports.ChatMessage = require('./ChatMessage');
