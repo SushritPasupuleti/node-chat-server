e536e3bf74ce5b786191b3c96468c915c50d067a
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 15:57:22 +0530

    Create ChatMessage.js

---
+++ server/models/ChatMessage.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+
+const MESSAGE_TYPES = {
+    TYPE_TEXT: "text",
+};
+
+const readByRecipientSchema = new mongoose.Schema(
+    {
+        _id: false,
+        readByUserId: String,
+        readAt: {
+            type: Date,
+            default: Date.now(),
+        },
+    },
+    {
+        timestamps: false,
+    }
+);
+
+const chatMessageSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            default: () => uuidv4().replace(/\-/g, ""),
+        },
+        chatRoomId: String,
+        message: mongoose.Schema.Types.Mixed,
+        type: {
+            type: String,
+            default: () => MESSAGE_TYPES.TYPE_TEXT,
+        },
+        postedByUser: String,
+        readByRecipients: [readByRecipientSchema],
+    },
+    {
+        timestamps: true,
+        collection: "chatmessages",
+    }
+);
+
+
+module.exports = mongoose.model("Chatmessage", chatRoomSchema);
