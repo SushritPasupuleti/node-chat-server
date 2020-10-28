250c811c1262a472f068b733b6ac89b790141819
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 17:10:54 +0530

    Update default.js

--- server/routes/default.js
+++ server/routes/default.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 
 router
-  .post('/login/:userId', () => {});
+  .post('/login/:userId', encode, (req, res, next) => {
+    return res
+      .status(200)
+      .json({
+        success: true,
+        authorization: req.authToken,
+      });
+  });
 
 module.exports = router;
