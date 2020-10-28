7a06a67e6db4214e8f5a7c145c823bb218c52575
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 16:23:56 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,8 +4,8 @@ const user = require('./../handlers/user');
 
 router
     .get('/', user.onGetAllUsers)
-    .post('/', () => { })
-    .get('/:id', () => { })
-    .delete('/:id', () => { })
+    .post('/', user.onCreateUser)
+    .get('/:id', user.onGetUserById)
+    .delete('/:id', user.onDeleteUserById)
 
 module.exports = router;
