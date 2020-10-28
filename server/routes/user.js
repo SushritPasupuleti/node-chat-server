c01370211a8e7ab7c4fc5db3ea832750ccca4649
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 15:42:12 +0530

    Create user.js

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router
+    .get('/', () => { })
+    .post('/', () => { })
+    .get('/:id', () => { })
+    .delete('/:id', () => { })
+
+module.exports = router;
