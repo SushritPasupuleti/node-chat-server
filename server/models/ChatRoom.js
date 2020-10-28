e2471f08894c8687919c18f6087e6ac6f094ef9a
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-28 15:53:28 +0530

    Create ChatRoom.js

---
+++ server/models/ChatRoom.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+
+const CHAT_ROOM_TYPES = {
+    CONSUMER_TO_CONSUMER: "consumer-to-consumer",
+    CONSUMER_TO_SUPPORT: "consumer-to-support",
+};
+
+const chatRoomSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: String,
+            default: () => uuidv4().replace(/\-/g, ""),
+        },
+        userIds: Array,
+        type: String,
+        chatInitiator: String,
+    },
+    {
+        timestamps: true,
+        collection: "chatrooms",
+    }
+);
+
+module.exports = mongoose.model("Chatroom", chatRoomSchema);
+module.exports = CHAT_ROOM_TYPES;
